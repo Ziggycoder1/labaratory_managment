c463fa0fb0b06a6e4b55235a29e0243b083c785b
Ziggycoder1 <148140113+Ziggycoder1@users.noreply.github.com>
2025-06-20 19:39:22 +0200

    change database

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'laboratory_db',
+  host: process.env.DB_HOST || 'nozomi.proxy.rlwy.net',
+  user: process.env.DB_USER || '49783',
+  password: process.env.DB_PASSWORD || 'OeIwknUlpfVVdsqtyAbEFSGmUePmtarA',
+  database: process.env.DB_NAME || 'railway',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
